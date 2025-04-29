7a51be0287f9b7ec101aff5606587c5e892b21f5
Pierre-Yves <pierre-yves.monnet@laposte.net>
2025-04-28 17:42:55 -0700

    Increase time to complete to 5 seconds

--- src/main/java/io/camunda/score/worker/GetScoreWorker.java
+++ src/main/java/io/camunda/score/worker/GetScoreWorker.java
@@ -27,7 +27,7 @@ public class GetScoreWorker {
         int score = Integer.parseInt(digitsOnly);
 
         try {
-            Thread.sleep(2000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             logger.error("Ask to interrupt the sleep");
             Thread.currentThread().interrupt();
